96bc87ea2c4c8a1624da88583dbca0d0a303fadd
Kam Figy <kam.figy@uniform.dev>
2021-05-03 14:34:32 -0700

    starter release

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  serverRuntimeConfig: {},
+  publicRuntimeConfig: {
+    gaTrackingId: process.env.GA_UA_ID,
+    segmentTrackingId: process.env.SEGMENT_ID,
+  },
+  trailingSlash: true,
+};
